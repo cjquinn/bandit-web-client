e06e59de99ba48cbe404fd607b49822ae743dd0e
Russell Bishop <hey@russellbishop.co.uk>
2018-04-08 17:37:40 +0100

    Using --no-photo for placeholders

--- src/components/PlayerPhoto.js
+++ src/components/PlayerPhoto.js
@@ -6,7 +6,8 @@ const PlayerPhoto = () => (
 
         <svg className="c-player-photo__knot u-z-1 u-pos-absolute u-top-0 u-left-0 u-width-1bl u-height-auto" width="36" height="31" viewBox="0 0 36 31"><use href="/dist/svg/sprite.svg#logo-knot"></use></svg>
 
-        <div className="c-player-photo u-width-2bl">
+        {/* Only add no-photo if the player has... no... photo.... */}
+        <div className="c-player-photo c-player-photo--no-photo u-width-2bl">
             <div className="c-player-photo__level u-bgcolor-god"></div>
 
             <div className="c-player-photo__level u-bgcolor-bandit"></div>
