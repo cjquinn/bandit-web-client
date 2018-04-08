6648c3f62541b5e9e20d90b4a95e4c0780ecb6b6
Russell Bishop <hey@russellbishop.co.uk>
2018-04-08 17:36:57 +0100

    Footer variations

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -9,6 +9,7 @@ const Footer = () => (
 
         <div class="o-container u-flex u-jc-between u-ai-center u-ph-1bl u-pv-2bl">
 
+            {/* Logged into club */}
             <select class="u-color-playdough">
                 <option selected="">Britannia Squash</option>
                 <optgroup label="Switch club:">
@@ -17,7 +18,21 @@ const Footer = () => (
                 </optgroup>
             </select>
 
-            <a href="/invite-player.php" class="u-color-playdough">Invite player</a>
+            <a href="/invite-player" class="u-color-playdough">Invite player</a>
+
+            
+
+            {/* Not logged into club
+            <a href="/sign-out" class="u-color-playdough">Sign out</a> */}
+
+
+            {/* Footer non-auth
+            <a href="/create-club-unauthenticated" class="u-color-playdough">Create a Club</a>
+            <a href="/request-password-reset" class="u-color-playdough">Reset password</a> */}
+
+
+            {/* Footer creating club or registering (not auth)
+            <a href="/sign-in" class="u-color-playdough">Sign in</a> */}
 
         </div>
 
