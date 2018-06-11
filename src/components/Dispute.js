7cb0de15b1cd7713e2cfb014c8b8b46d00b14754
Russell Bishop <hey@russellbishop.co.uk>
2018-06-11 18:18:05 +0100

    Removing faces from disputes differentiates them - much easier to spot

--- src/components/Dispute.js
+++ src/components/Dispute.js
@@ -8,10 +8,7 @@ const Dispute = () => (
     <li className="u-bgcolor-fold u-pos-relative">
         <div className="o-absolute-fill u-borrad-inherit u-shadow-dispute u-pointer-none"></div>
         <a href="/result" className="u-flex u-ai-center u-pv-1bl u-ph-1bl">
-            <div className="u-mr-1bl">
-                <PlayerPhoto />
-            </div>
-            <div className="u-grow-1 u-vspace-03r">
+            <div className="u-vspace-03r">
                 <dt className="u-color-paste"><span className="o-dictate">Dispute with </span>Rebekah Reeves</dt>
                 <dd className="u-size-14px u-color-hotmelon"><span className="o-dictate">You have </span>4 hours to respond</dd>
             </div>
