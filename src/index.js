52d7975b58bca4580c5dbdf13c8f2e037c6bfe44
Russell Bishop <hey@russellbishop.co.uk>
2018-07-02 18:41:50 +0100

    Starting, and breaking things..

--- src/index.js
+++ src/index.js
@@ -12,17 +12,25 @@ import Root from './containers/Root';
 // Store
 import configureStore from './store/configureStore';
 
+// Mixpanel
+import mixpanel from 'mixpanel-browser';
+import MixpanelProvider from 'react-mixpanel';
+
 // Stylesheet
 import './assets/scss/main.scss';
+
+mixpanel.init('0f22e3afaf2b537e2fb1b19a4276de60');
 
 const history = createHistory();
 const store = configureStore({user: {clubId: getClubId()}}, history);
 
 const render = Component => {
     ReactDOM.render(
-        <AppContainer>
-            <Component history={history} store={store} />
-        </AppContainer>,
+        <MixpanelProvider mixpanel={mixpanel}>
+            <AppContainer>
+                <Component history={history} store={store} />
+            </AppContainer>
+        </MixpanelProvider>,
         document.getElementById('root')
     );
 };
