51e298568b6f46b0fe47d037e65aa9f1d465c040
Christy Quinn <christy@wearelighthouse.com>
2018-09-25 20:32:45 +0100

    Correct public path

--- webpack.production.config.js
+++ webpack.production.config.js
@@ -48,7 +48,7 @@ module.exports = {
                     {
                         loader: 'file-loader',
                         options: {
-                            publicPath: '/',
+                            publicPath: '/img/',
                             outputPath: 'img/'
                         }
                     }
