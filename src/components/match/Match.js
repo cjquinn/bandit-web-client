058e670a2224f2ba6c0eaf3bb2daf648281240d7
Russell Bishop <hey@russellbishop.co.uk>
2019-04-19 14:30:18 +0100

    Static version of Single Match V2

--- src/components/match/Match.js
+++ src/components/match/Match.js
@@ -20,9 +20,9 @@ const Match = ({ handleClickCancel, isDeleting, match, userId }) => (
             }
 
             <dl className={`u-flex u-hspace-1px u-borrad-first-2002 u-borrad-last-0220 ${match.deleted && 'u-opac-05'}`}>
-                <li className="u-flex u-fd-col u-grow-1 u-basis-0 u-ai-center u-pv-3bl u-bgcolor-fold">
+                <li className="u-grow-1 u-basis-0 u-pt-3bl u-bgcolor-fold">
 
-                    <div className="u-flex u-fd-col u-ai-center u-vspace-1bl">
+                    <div className="u-flex u-fd-col u-ai-center u-pv-3bl u-vspace-1bl">
                         <Link to={`/players/${match.player_a_id}`}>
                             <PlayerPhoto
                                 player={match.player_a}
@@ -31,7 +31,7 @@ const Match = ({ handleClickCancel, isDeleting, match, userId }) => (
                         </Link>
 
                         <dt>
-                            <h2 className="u-color-paste u-capitalize">
+                            <h2 className="u-weight-bold u-color-paste u-capitalize">
                                 <Link to={`/players/${match.player_a_id}`}>
                                     {match.player_a.user.first_name} {match.player_a.user.last_name}
                                 </Link>
@@ -45,32 +45,41 @@ const Match = ({ handleClickCancel, isDeleting, match, userId }) => (
                         </dd>
                     </div>
 
-                    <span className={`c-levels u-flex u-ai-center`}>
-                        <span className={`c-level u-bgcolor-warrior`}>
-                            Warrior
+                    <div className="u-flex u-fd-col u-ai-center u-vspace-05bl u-bgcolor-obsidian u-pv-2bl">
+
+                        <span className={`c-levels u-flex u-ai-center`}>
+                            <span className={`c-level u-bgcolor-warrior`}>
+                                Warrior
+                            </span>
                         </span>
-                    </span>
 
-                    <div className="u-flex u-ai-center u-hspace-4px">
-                        <Svg
-                            sprite={rating}
-                            className="u-width-1bl u-height-auto"
-                        />
+                        <div className="u-flex u-ai-center u-hspace-4px">
+                            <Svg
+                                sprite={rating}
+                                className="u-width-1bl u-height-auto"
+                            />
 
-                        <span className="u-color-paste">{match.player_a.rating} <span className="o-dictate">rating</span></span>&nbsp;
-                    
-                        {match.player_a_snapshot &&
-                            <dd className={`u-color-${match.player_a_snapshot.difference >= 0 ? 'win' : 'loss'}`}>
-                                {match.player_a_snapshot.difference >= 0 ? '+' : ''}
-                                {match.player_a_snapshot.difference} <abbr title="rating points">pts.</abbr>
-                            </dd>
-                        }
+                            <span className="u-color-paste">{match.player_a.rating} <span className="o-dictate">rating</span></span>&nbsp;
+                        
+                            {match.player_a_snapshot &&
+
+                                // No idea how to do this efficiently :(
+                                    
+                                <dd className=
+                                {`c-points c-points--match c-points--${match.player_a_snapshot.difference === 0 ? 'neutral' : (match.player_a_snapshot.difference > 0 ? 'win' : 'loss')}`}
+                                
+                                title="rating points">
+                                    {match.player_a_snapshot.difference >= 0 ? '+' : ''}
+                                    {match.player_a_snapshot.difference}
+                                </dd>
+                            }
+                        </div>
                     </div>
                 </li>
 
-                <li className="u-flex u-fd-col u-grow-1 u-basis-0 u-ai-center u-pv-3bl u-bgcolor-fold05">
+                <li className="u-grow-1 u-basis-0 u-ai-center u-bgcolor-fold">
 
-                    <div className="u-flex u-fd-col u-ai-center u-vspace-1bl">
+                    <div className="u-flex u-fd-col u-ai-center u-pv-3bl u-vspace-1bl">
                         <Link to={`/players/${match.player_b_id}`}>
                             <PlayerPhoto
                                 player={match.player_b}
@@ -79,7 +88,7 @@ const Match = ({ handleClickCancel, isDeleting, match, userId }) => (
                         </Link>
 
                         <dt>
-                            <h2 className="u-color-paste u-capitalize">
+                            <h2 className="u-weight-bold u-color-paste u-capitalize">
                                 <Link to={`/players/${match.player_b_id}`}>
                                     {match.player_b.user.first_name} {match.player_b.user.last_name}
                                 </Link>
@@ -93,38 +102,236 @@ const Match = ({ handleClickCancel, isDeleting, match, userId }) => (
                         </dd>
                     </div>
 
-                        
+                    <div className="u-flex u-fd-col u-ai-center u-vspace-05bl u-bgcolor-obsidian u-pv-2bl">
 
-                    <span className={`c-levels u-flex u-ai-center`}>
-                        <span className={`c-level u-bgcolor-warrior`}>
-                            Warrior
+                        <span className={`c-levels u-flex u-ai-center`}>
+                            <span className={`c-level u-bgcolor-warrior`}>
+                                Warrior
+                            </span>
                         </span>
-                    </span>
 
-                    <div className="u-flex u-ai-center u-hspace-4px">
-                        <Svg
-                            sprite={rating}
-                            className="u-width-1bl u-height-auto"
-                        />
+                        <div className="u-flex u-ai-center u-hspace-4px">
+                            <Svg
+                                sprite={rating}
+                                className="u-width-1bl u-height-auto"
+                            />
 
-                        <span className="u-color-paste">{match.player_b.rating} <span className="o-dictate">rating</span></span>&nbsp;
+                            <span className="u-color-paste">{match.player_b.rating} <span className="o-dictate">rating</span></span>&nbsp;
 
-                        {match.player_b_snapshot &&
-                            <dd className={`u-color-${match.player_b_snapshot.difference >= 0 ? 'win' : 'loss'}`}>
-                                {match.player_b_snapshot.difference >= 0 ? '+' : ''}
-                                {match.player_b_snapshot.difference} <abbr title="rating points">pts.</abbr>
-                            </dd>
-                        }
+                            {match.player_b_snapshot &&
+                                // No idea how to do this efficiently :(
+                                    
+                                <dd className=
+                                {`c-points c-points--match c-points--${match.player_b_snapshot.difference === 0 ? 'neutral' : (match.player_b_snapshot.difference > 0 ? 'win' : 'loss')}`}
+                                
+                                title="rating points">
+                                    {match.player_b_snapshot.difference >= 0 ? '+' : ''}
+                                    {match.player_b_snapshot.difference}
+                                </dd>
+                            }
+                        </div>
+
                     </div>
                 </li>
             </dl>
 
-            <section>
+            <section className="u-vspace-1bl">
                 
                 <div className="u-ph-1bl">
-                    <h3 className="u-size-h4 u-color-white">Match</h3>
+                    <h3 className="u-weight-bold u-size-h3 u-color-white">Match</h3>
                 </div>
 
+                <div className="c-match-table">
+
+                    <div className="c-match-table__head">
+                        <h4 className="c-match-table__subtitle">Games</h4>
+                    </div>
+
+                    <ol className="c-match-table__column">
+                        <li className="c-match-table__cell u-size-12px u-jc-between u-ai-center">
+                            <span className="o-dictate">Russell</span>
+                            <span className="">Won</span>
+                            <span className="c-points c-points--game c-points--win">+2</span>
+                        </li>
+
+                        <li className="c-match-table__cell u-size-12px u-jc-between u-ai-center">
+                            <span className="o-dictate">Russell</span>
+                            <span className="">Won</span>
+                            <span className="c-points c-points--game c-points--win">+2</span>
+                        </li>
+
+                        <li className="c-match-table__cell u-size-12px u-jc-between u-ai-center">
+                            <span className="o-dictate">Russell</span>
+                            <span className="">Lost</span>
+                            <span className="c-points c-points--game c-points--loss">-6</span>
+                        </li>
+
+                        <li className="c-match-table__foot">
+                            <span className="c-match-table__subtitle">Total</span>
+                            <span className="u-ml-auto">
+                                <span className="c-points c-points--match c-points--loss">-2</span>
+                            </span>
+                        </li>
+                    </ol>
+
+                    <ol className="c-match-table__column">
+                        <li className="c-match-table__cell u-size-12px u-jc-between u-ai-center">
+                            <span className="o-dictate">Russell</span>
+                            <span className="">Won</span>
+                            <span className="c-points c-points--game c-points--win">+6</span>
+                        </li>
+
+                        <li className="c-match-table__cell u-size-12px u-jc-between u-ai-center">
+                            <span className="o-dictate">Russell</span>
+                            <span className="">Lost</span>
+                            <span className="c-points c-points--game c-points--loss">-2</span>
+                        </li>
+
+                        <li className="c-match-table__cell u-size-12px u-jc-between u-ai-center">
+                            <span className="o-dictate">Russell</span>
+                            <span className="">Lost</span>
+                            <span className="c-points c-points--game c-points--loss">-2</span>
+                        </li>
+
+                        <li className="c-match-table__foot">
+                            <span className="o-dictate">Total</span>
+                            <span className="u-ml-auto">
+                                <span className="c-points c-points--match c-points--win">+2</span>
+                            </span>
+                        </li>
+                    </ol>
+
+                </div>
+                
+            </section>
+
+            <section className="u-vspace-1bl">
+                
+                <div className="u-ph-1bl">
+                    <h3 className="u-weight-bold u-size-h3 u-color-white">Ratings</h3>
+                </div>
+
+                <div className="c-match-table">
+
+                    <div className="c-match-table__head">
+                        <h4 className="c-match-table__subtitle">Last Rating</h4>
+                    </div>
+
+                    <ol className="c-match-table__column">
+                        <li className="c-match-table__cell u-fd-col u-vspace-05bl">
+                            <span className={`c-levels u-flex u-ai-center`}>
+                                <span className={`c-level u-bgcolor-warrior`}>
+                                    Warrior
+                                </span>
+                            </span>
+
+                            <div className="u-flex u-ai-center u-hspace-4px">
+                                <Svg
+                                    sprite={rating}
+                                    className="u-width-1bl u-height-auto"
+                                />
+
+                                <span className="u-color-paste">{match.player_a.rating} <span className="o-dictate">rating</span></span>
+                            </div>
+                        </li>
+
+                        {/* <li className="c-match-table__cell u-fd-col">
+                            <dt className="u-color-paste">Low Volatility</dt>
+                            <dd className="u-color-steam u-size-13px">Unrated Opponent</dd>
+                        </li> */}
+                    </ol>
+
+                    <ol className="c-match-table__column">
+                        <li className="c-match-table__cell u-fd-col u-vspace-05bl">
+                            <span className={`c-levels u-flex u-ai-center`}>
+                                <span className={`c-level u-bgcolor-warrior`}>
+                                    Warrior
+                                </span>
+                            </span>
+
+                            <div className="u-flex u-ai-center u-hspace-4px">
+                                <Svg
+                                    sprite={rating}
+                                    className="u-width-1bl u-height-auto"
+                                />
+
+                                <span className="u-color-paste">{match.player_a.rating} <span className="o-dictate">rating</span></span>
+                            </div>
+                        </li>
+
+                        {/* <li className="c-match-table__cell u-fd-col">
+                            <dt className="u-color-paste">High Volatility</dt>
+                            <dd className="u-color-steam u-size-13px">New Player</dd>
+                        </li> */}
+                    </ol>
+
+                    <div className="c-match-table__head">
+                        <h4 className="c-match-table__subtitle">New Rating</h4>
+                    </div>
+
+                    <ol className="c-match-table__column">
+                        <li className="c-match-table__cell u-fd-col u-vspace-05bl">
+                            <span className={`c-levels u-flex u-ai-center`}>
+                                <span className={`c-level u-bgcolor-warrior`}>
+                                    Warrior
+                                </span>
+                            </span>
+
+                            <div className="u-flex u-ai-center u-hspace-4px">
+                                <Svg
+                                    sprite={rating}
+                                    className="u-width-1bl u-height-auto"
+                                />
+
+                                <span className="u-color-paste">{match.player_a.rating} <span className="o-dictate">rating</span></span>&nbsp;
+
+                                {match.player_a_snapshot &&
+                                    // No idea how to do this efficiently :(
+                                        
+                                    <dd className=
+                                    {`c-points c-points--match c-points--${match.player_a_snapshot.difference === 0 ? 'neutral' : (match.player_a_snapshot.difference > 0 ? 'win' : 'loss')}`}
+                                    
+                                    title="rating points">
+                                        {match.player_a_snapshot.difference >= 0 ? '+' : ''}
+                                        {match.player_a_snapshot.difference}
+                                    </dd>
+                                }
+                            </div>
+                        </li>
+                    </ol>
+
+                    <ol className="c-match-table__column">
+                        <li className="c-match-table__cell u-fd-col u-vspace-05bl">
+                            <span className={`c-levels u-flex u-ai-center`}>
+                                <span className={`c-level u-bgcolor-warrior`}>
+                                    Warrior
+                                </span>
+                            </span>
+
+                            <div className="u-flex u-ai-center u-hspace-4px">
+                                <Svg
+                                    sprite={rating}
+                                    className="u-width-1bl u-height-auto"
+                                />
+
+                                <span className="u-color-paste">{match.player_b.rating} <span className="o-dictate">rating</span></span>&nbsp;
+
+                                {match.player_b_snapshot &&
+                                    // No idea how to do this efficiently :(
+                                        
+                                    <dd className=
+                                    {`c-points c-points--match c-points--${match.player_b_snapshot.difference === 0 ? 'neutral' : (match.player_b_snapshot.difference > 0 ? 'win' : 'loss')}`}
+                                    
+                                    title="rating points">
+                                        {match.player_b_snapshot.difference >= 0 ? '+' : ''}
+                                        {match.player_b_snapshot.difference}
+                                    </dd>
+                                }
+                            </div>
+                        </li>
+                    </ol>
+
+                </div>
                 
             </section>
 
@@ -147,22 +354,16 @@ const Match = ({ handleClickCancel, isDeleting, match, userId }) => (
                 </div>
             </details> */}
 
-            <dl className="u-ph-1bl u-vspace-1bl">
-                <li className="u-flex u-ai-center u-hspace-05bl u-fw-wrap">
-                    <dt>Submitted by </dt>
-                    <dd className="u-flex u-ai-center u-hspace-05bl">
-                        <PlayerPhoto 
-                            player={match.player_a} 
-                            width="2bl"
-                        /> <span className="u-capitalize">{match.player_a.user.first_name} {match.player_a.user.last_name}</span>
-                    </dd>
-                </li>
-
+            <dl className="u-ph-1bl u-vspace-05bl">
                 <li>
                     <dt className="o-dictate">Submitted on </dt>
                     <dd>
                         {moment(match.created).format('dddd Do MMMM YYYY')}
                     </dd>
+                </li>
+
+                <li className="u-flex u-ai-center u-hspace-05bl u-size-12px u-color-steam">
+                    <dt>Submitted by</dt><dd className=""><span className="u-capitalize">{match.player_a.user.first_name} {match.player_a.user.last_name}</span></dd>
                 </li>
             </dl>
         </div>
