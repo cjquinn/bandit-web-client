9e3c0d981a74574380a35f2015960dcf3ddbb201
Christy Quinn <christyjquinn@gmail.com>
2019-05-16 20:22:58 +0100

    Seperated out match into components

---
+++ src/components/match/Match/Rating.js
@@ -0,0 +1,48 @@
+import PropTypes from 'prop-types';
+import React from 'react';
+
+// Components
+import Svg from '../../shared/Svg';
+import Template from '../../shared/Template';
+
+// Sprite
+import rating from '../../../assets/svg/sprite/rating.svg';
+
+const Rating = ({ level, playerRating, snapshot }) => (
+    <Template>
+        {level &&
+            <span className="c-levels u-flex u-ai-center">
+                <span className={`c-level u-bgcolor-${level.slug}`}>
+                    {level.name}
+                </span>
+            </span>
+        }
+
+        <div className="u-flex u-ai-center u-hspace-4px">
+            <Svg
+                sprite={rating}
+                className="u-width-1bl u-height-auto"
+            />
+
+            <span className="u-color-paste">{playerRating} <span className="o-dictate">rating</span></span>&nbsp;
+        
+            {snapshot &&
+                <dd
+                    className={`c-points c-points--match c-points--${snapshot.difference === 0 ? 'neutral' : (snapshot.difference > 0 ? 'win' : 'loss')}`}
+                    title="rating points"
+                >
+                    {snapshot.difference > 0 ? '+' : ''}
+                    {snapshot.difference}
+                </dd>
+            }
+        </div>
+    </Template>
+);
+
+Rating.propTypes = {
+    level: PropTypes.object,
+    playerRating: PropTypes.number.isRequired,
+    snapshot: PropTypes.object
+};
+
+export default Rating;
