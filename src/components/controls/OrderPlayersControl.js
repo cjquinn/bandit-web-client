da2b47ac6af1140609131db6f77bd4a0d9a8c7c5
Russell Bishop <hey@russellbishop.co.uk>
2020-02-21 08:50:43 +0000

    Testing new OrderPlayersControl

--- src/components/controls/OrderPlayersControl.js
+++ src/components/controls/OrderPlayersControl.js
@@ -1,20 +1,32 @@
 import PropTypes from 'prop-types';
 import React from 'react';
 
+// Sprites
+import { ReactComponent as ArrowDown } from '../../assets/svg/sprite/arrow_down.svg';
+
 const OrderPlayersControl = ({ handleChange, orderBy }) => (
     <div className="u-ph-1bl">
-        <div className="c-select">
-            <select
-                className="c-select__select u-color-playdough"
-                onChange={handleChange}
-                value={orderBy}
-            >
-                <optgroup label="Order players:">
-                    <option value="a-z">A &ndash; Z</option>
-                    <option value="rating">Highest Rating</option>
-                    <option value="games">Most Games</option>
-                </optgroup>
-            </select>
+
+        <div className="u-pos-relative">
+            <div className="c-select-field">
+                <div className="c-select-field__select">
+                    <div className="c-select-field__icon">
+                        <ArrowDown className="c-select-field__svg" />
+                    </div>
+    
+                    <select
+                        className="c-select-field__input u-capitalize"
+                        onChange={handleChange}
+                        value={orderBy}
+                    >
+                        <optgroup label="Order players:">
+                            <option value="a-z">A &ndash; Z</option>
+                            <option value="rating">Highest Rating</option>
+                            <option value="games">Most Games</option>
+                        </optgroup>
+                    </select>
+                </div>
+            </div>
         </div>
     </div>
 );
